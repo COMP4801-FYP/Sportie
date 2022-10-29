fd33a3e57e224852d0fc407941afe37ca2de1341
michael <marcell.michael27@gmail.com>
2022-10-29 17:58:08 +0800

    Connected Firebase functions to the app

--- app/src/main/java/hk/hkucs/sportieapplication/MainActivity.kt
+++ app/src/main/java/hk/hkucs/sportieapplication/MainActivity.kt
@@ -13,9 +13,15 @@ import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.firestore.DocumentReference
 
 import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var functions: FirebaseFunctions;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +30,40 @@ class MainActivity : AppCompatActivity() {
         val inputFirstName = findViewById<EditText>(R.id.inputFirstName);
         val inputLastName = findViewById<EditText>(R.id.inputLastName);
 
+        functions = Firebase.functions
+
         val saveButton = findViewById<Button>(R.id.saveButton);
         saveButton.setOnClickListener{
             val firstName = inputFirstName.text.toString();
             val lastName = inputLastName.text.toString();
             saveFireStore(firstName, lastName);
+            val temp: Task<String> = addMessage(inputFirstName.text.toString());
+            temp.addOnSuccessListener {
+                Log.d("TAG",temp.getResult())
+
+            }
         }
 
 
+
+    }
+    private fun addMessage(text: String): Task<String> {
+        // Create the arguments to the callable function.
+        val data = hashMapOf(
+            "text" to text,
+            "push" to true
+        )
+
+        return functions
+            .getHttpsCallable("addMessage")
+            .call(data)
+            .continueWith { task ->
+                // This continuation runs on either success or failure, but if the task
+                // has failed then result will throw an Exception which will be
+                // propagated down.
+                val result = task.result?.data as String
+                result
+            }
     }
 
     fun saveFireStore(firstName: String, lastName: String){
